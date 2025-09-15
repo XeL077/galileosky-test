63432847293aa75e490623bbc9b85511f421ee00
XeL077 <ale21111681@yandex.ru>
2025-09-16 00:09:53 +0300

    add default preset

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   const [parsedObject, setParsedObject] = useState({numbersToken: [], others: []});
   const textareaRef = useRef(null);
 
-  const handleClick = () => {
+  const handleClickSubmitBtn = () => {
     const textAreaValue = textareaRef.current?.value || '';
     const tokens = toSplitAreaValue(textAreaValue);
     const parsedObject = filterTokens(tokens);
@@ -17,11 +17,28 @@ export default function App() {
     }
   }
 
+  const handleClickSetPreset = (event) => {
+    const value = event.target.value;
+    if (value) {
+      textareaRef.current.value = value;
+    }
+  };
+
   return (
     <div className="app">
+      <MapLegend items={parsedObject.numbersToken} />
+      <textarea ref={textareaRef} placeholder="Type your message here." defaultValue={"2, 1, 4 5"} />
+      <button type="button" onClick={handleClickSubmitBtn}>Показать</button>
 
-      <textarea ref={textareaRef} placeholder="Type your message here." defaultValue={'1 2, 4, 10 40'} />
-      <button type="button" onClick={handleClick}>Показать</button>
+      <div>
+        <p>
+          Значения по-умолчанию
+        </p>
+        <input type='button' onClick={handleClickSetPreset} value='3 2, 4, 10 40' />
+        <input type='button' onClick={handleClickSetPreset} value='1, 2 
+        d 40d' />
+        
+      </div>
       <Statistics 
         countNumbersToken={parsedObject.numbersToken.length} 
         filteredCount={parsedObject.others.length} 
